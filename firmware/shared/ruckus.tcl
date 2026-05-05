f0beac24604c1f465ce1b7f15d888e9e2404d791
Bela Urbschat <b.urbschat@tum.de>
2026-05-05 18:54:00 +0900

    -sim_only flag must come first for vivado build
    Vivado build tcl scripts expect the option to come first it seems...

--- firmware/shared/ruckus.tcl
+++ firmware/shared/ruckus.tcl
@@ -7,10 +7,10 @@ loadRuckusTcl $::env(TOP_DIR)/submodules/axi-soc-ultra-plus-core/hardware/RealDi
 
 # Load RTL code
 loadSource -dir "$::DIR_PATH/rtl"
-loadSource -dir -sim_only "$::DIR_PATH/tb" -fileType "VHDL 2008"
+loadSource -sim_only -dir "$::DIR_PATH/tb" -fileType "VHDL 2008"
 # Load VIVADO unisim components (not sure if this is correct...)
-loadSource -lib unisim -path -sim_only "$::env(XILINX_VIVADO)/data/vhdl/src/unisims/unisim_VCOMP.vhd"
-loadSource -lib unisim -dir -sim_only "$::env(XILINX_VIVADO)/data/vhdl/src/unisims/primitive"
+loadSource -sim_only -lib unisim -path "$::env(XILINX_VIVADO)/data/vhdl/src/unisims/unisim_VCOMP.vhd"
+loadSource -sim_only -lib unisim -dir  "$::env(XILINX_VIVADO)/data/vhdl/src/unisims/primitive"
 
 # Load IP cores
 loadIpCore -dir "$::DIR_PATH/ip"
